ffb5b51fa481145776f4803e89ea592e67ab6a49
Shellica Bethune <shelliciabethune9@gmail.com>
2025-04-21 22:07:10 -0400

    Remove unused import to fix ESLint error

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from './context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { addTask, deleteTask, editTask } from './api/azureFunctions';
+import { addTask, deleteTask,} from './api/azureFunctions';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import './Dashboard.css';
 
